64dd3cf9d8850d53875468dc1a0c60b9a937565e
Timothy Cole <tim@timcole.me>
2017-09-23 09:54:40 -0400

    WeetBot points endpoint

--- routes/api/weetbot.js
+++ routes/api/weetbot.js
@@ -16,4 +16,20 @@ router.get('/weetbot/commands', async function(req, res, next) {
 	res.status(200).json({ status: 200, commands });
 });
 
+router.get('/weetbot/points', async function(req, res, next) {
+	res.setHeader('Content-Type', 'application/json');
+
+	var redis = req.app.get('redis');
+	var pointKeys = await redis.keysAsync(`${process.env.WEETBOT_POINTS_KEY}*`);
+	var chatters = {};
+
+	for (var i = 0; i < pointKeys.length; i++) {
+		var points = await redis.getAsync(pointKeys[i]);
+		if (points) chatters[pointKeys[i].replace(process.env.WEETBOT_POINTS_KEY, "")] = points;
+		// if (points) chatters.push({'twitch_id': pointKeys[i].replace(process.env.WEETBOT_POINTS_KEY, ""), points});
+	}
+
+	res.status(200).json({ status: 200, chatters });
+});
+
 module.exports = router;
